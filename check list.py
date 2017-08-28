694ba613d494cf7b6293c38141456805abf064e6
Lorence Cheng <lorence123@outlook.com>
2017-08-28 21:51:58 +0800

    Add folder check code
    Check excel file exist before run program

--- check list.py
+++ check list.py
@@ -1,9 +1,11 @@
 # test the logic of github
-''' I need to try to code to load all sheet from my original Excel check list,
-     it is seperated by different sheet.'''
-
+'''
+    20170827: I need to try to code to load all sheet from my original Excel check list, it is seperated by different sheet.
+    20170828: I try to move the database excle file to another folder, then let program judge whether the folder exist
+'''
 
 import openpyxl
+import os
 
 # define a function to print any sheet items
 def print_check_point(sheet_name):
@@ -13,9 +15,19 @@ def print_check_point(sheet_name):
         print(str(ws["A"+str(row)].value)," : " , str(ws["B"+str(row)].value))
 
 #Start the main code
-wb=openpyxl.load_workbook("database checking items.xlsx",data_only=True)
-sheet_name=[]
-for sheet_name in wb.get_sheet_names():
-    print_check_point(sheet_name)
-    print("----------------------------------------"+str(sheet_name) + "-----Finish---------------------------------------")
-    print("")
+current_path=os.getcwd()
+print("Python Program in this folder: " + str(current_path))
+print("")
+database_folder="C:/Users/Lorence/Desktop/excel test"  # place the Excel file in this folder, need update if the file location change.
+try:
+    if os.path.exists(database_folder):
+        os.chdir(database_folder)
+    wb=openpyxl.load_workbook("database checking items.xlsx",data_only=True)
+    sheet_name=[]
+    for sheet_name in wb.get_sheet_names():
+        print_check_point(sheet_name)
+        print("----------------------------------------"+str(sheet_name) + "-----Finish---------------------------------------")
+        print("")
+
+except IOError:    #if no this excel folder, alarm
+    print("Sorry, I can't find the database Excel file! Please check folder: " + database_folder)
